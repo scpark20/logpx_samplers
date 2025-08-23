99ce3bdeac53fd36689b4f093b83af8a0d256aba
soochul park <scpark20@gmail.com>
2025-08-24 05:55:59 +0900

    last

--- training/ablations/pred_corr/train_gdual.py
+++ training/ablations/pred_corr/train_gdual.py
@@ -43,7 +43,7 @@ config.latent_size   = (4, 32, 32)
 # LR & Scheduler
 config.base_lr       = 2e-3
 config.end_lr        = 1e-4
-config.total_steps   = 10*1000        # 전체 학습 스텝
+config.total_steps   = 10*1000+1        # 전체 학습 스텝
 
 # ---- 여기만 CLI로 덮어씀 ----
 config.n_steps       = args.n_steps
