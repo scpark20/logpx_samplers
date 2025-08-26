d4979304fea7851a86df6678cc61a285be7e9976
soochul park <scpark20@gmail.com>
2025-08-27 00:22:33 +0900

    sh

---
+++ training/ablations/temperature/train_gdual_all.sh
@@ -0,0 +1,8 @@
+./training/ablations/temperature/train_gdual0.sh &
+./training/ablations/temperature/train_gdual1.sh &
+./training/ablations/temperature/train_gdual2.sh &
+./training/ablations/temperature/train_gdual3.sh &
+./training/ablations/temperature/train_gdual4.sh &
+./training/ablations/temperature/train_gdual5.sh &
+./training/ablations/temperature/train_gdual6.sh &
+./training/ablations/temperature/train_gdual7.sh &
