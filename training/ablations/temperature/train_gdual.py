203a8c14f6e7e1029d0c3126636f968395934a85
soochul park <scpark20@gmail.com>
2025-08-26 23:31:04 +0900

    checkpoint

--- training/ablations/temperature/train_gdual.py
+++ training/ablations/temperature/train_gdual.py
@@ -92,7 +92,7 @@ solver = GDual_Solver(
     use_corrector=True,
     time_learning=True,
     train_mode=True,
-    #checkpoint=True
+    checkpoint=True
 ).to(device)
 
 optimizer = torch.optim.AdamW(solver.parameters(), lr=config.base_lr)
