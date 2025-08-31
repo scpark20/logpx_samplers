7480ce50fb4f10e150fa4f9e155807cacc397059
scpark <scpark20@gmail.com>
2025-09-01 05:22:59 +0900

    ds

--- solvers/competing/ds/ds_solver_flow.py
+++ solvers/competing/ds/ds_solver_flow.py
@@ -12,11 +12,11 @@ class DS_Solver(Solver):
         steps,
         skip_type="time_uniform_flow",
         flow_shift=1.0,
-        algorithm_type="dual_prediction",
+        algorithm_type="vector_prediction",
         checkpoint=False,
         **kwargs
     ):
-        assert algorithm_type == 'dual_prediction'
+        assert algorithm_type == 'vector_prediction'
         super().__init__(noise_schedule, algorithm_type)
 
         self.steps = steps
@@ -28,41 +28,34 @@ class DS_Solver(Solver):
         t_T = noise_schedule.T
         timesteps = self.get_time_steps(skip_type=skip_type, t_T=t_T, t_0=t_0, N=steps, device='cpu', shift=flow_shift)
         self.log_deltas = nn.Parameter(torch.log(timesteps[:-1] - timesteps[1:]))
-        self.M = nn.Parameter(torch.zeros(steps, steps))
+        self.M = nn.Parameter(torch.eye(steps) * 0)
         
     def sample(self, x, model_fn, output_traj=False, **kwargs):
         self.set_model_fn(model_fn)
         
         device, dtype = x.device, x.dtype
         timesteps = self.learned_timesteps(device=device, dtype=dtype)  # <-- 학습된 ts
-        # noise_schedule이 텐서를 받아들일 수 있어야 자동미분이 유지됩니다.
-        alphas = self.noise_schedule.marginal_alpha(timesteps)          # 벡터화된 구현 권장
-        sigmas = self.noise_schedule.marginal_std(timesteps)
-        delta_alphas = alphas[1:] - alphas[:-1]
-        delta_sigmas = sigmas[1:] - sigmas[:-1]
+        dt = timesteps[1:] - timesteps[:-1]
         
-        vs = []
-        trajs = [x]
+        solver_coeffs = self.M
+        pred_trajectory = []
+        trajs = [x,]
         for i in tqdm(range(self.steps), disable=os.getenv("TQDM", "False")):
-            xc, ec = self.checkpoint_model_fn(x, timesteps[i]) if self.checkpoint else self.model_fn(x, timesteps[i])
-            vc = delta_alphas[i]*xc + delta_sigmas[i]*ec
-            if i == 0:
-                x = x + vc
-            else:
-                # (i-1,)
-                C = self.M[i, :i]
-                if (self.steps <= 6) and i >= self.steps-2:
-                    mask = torch.zeros_like(C); mask[-1] = 1.
-                    C = C * mask
-                    
+            vc = self.checkpoint_model_fn(x, timesteps[i]) if self.checkpoint else self.model_fn(x, timesteps[i])
+            pred_trajectory.append(vc)
 
-                c_diag = 1 - torch.sum(C)
-                V = torch.stack(vs, dim=0) # (N, b, c, h, w)
-                x = x + c_diag*vc + torch.sum(C[:, None, None, None, None]*V, dim=0)
-            vs.append(vc)
-            if output_traj:
-                trajs.append(x)    
-
+            velocity = torch.zeros_like(vc)
+            sum_solver_coeff = 0.0
+            for j in range(i):
+                if self.steps <= 6 and i == self.steps - 2 and j != i - 1: continue
+                if self.steps <= 6 and i == self.steps - 1 and j != i - 1: continue
+                velocity += solver_coeffs[i, j] * pred_trajectory[j]
+                sum_solver_coeff += solver_coeffs[i, j]
+                
+            velocity += (1 - sum_solver_coeff) * pred_trajectory[-1]
+            x = x + velocity*dt[i]
+            trajs.append(x)
+            
         outputs = {'samples': x}
         if output_traj:
             outputs['traj'] = torch.stack(trajs, dim=1)
