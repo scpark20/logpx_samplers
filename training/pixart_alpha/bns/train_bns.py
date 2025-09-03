158b2d15f70c5d54aa3b4b65d4c60028fde4ccc8
scpark <scpark20@gmail.com>
2025-09-03 13:21:36 +0900

    bns

--- training/pixart_alpha/bns/train_bns.py
+++ training/pixart_alpha/bns/train_bns.py
@@ -76,7 +76,7 @@ solver = BNS_Solver(noise_schedule,
         config.n_steps,
         skip_type='time_uniform',
         algorithm_type='dual_prediction',
-        checkpoint=False).to(device)
+        checkpoint=True).to(device)
 optimizer = torch.optim.AdamW(solver.parameters(), lr=config.base_lr)
 
 # ---- Scheduler: Pure Cosine ----
