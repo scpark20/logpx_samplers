034545df65b08952ecd21b159c6aa02fb0ae781f
scpark <scpark20@gmail.com>
2025-09-04 17:45:38 +0900

    sana_traj

--- training/sana/traj/sample_10k.sh
+++ training/sana/traj/sample_10k.sh
@@ -9,21 +9,20 @@ export OMP_NUM_THREADS=${OMP_NUM_THREADS:-4}
 export MKL_NUM_THREADS=${MKL_NUM_THREADS:-4}
 export OPENBLAS_NUM_THREADS=${OPENBLAS_NUM_THREADS:-4}
 
-TAG=dual
-MODEL=GMDiT
-DATA=ImageNet
-BATCH_SIZE=10          # GPU당 배치
-ALGO=dual_prediction
+TAG=ds
+MODEL=SANA
+DATA=MSCOCO2014_valid
+BATCH_SIZE=1          # GPU당 배치
+ALGO=vector_prediction
 SKIP=time_uniform_flow
-FLOW_SHIFT=1.0
+FLOW_SHIFT=3.0
 ORDER=2
-N_SAMPLES=50000
+N_SAMPLES=10000
 SEED_OFFSET=0
 
-SOLVERS=("Dual-Solver")
-NFES=(9 7 5 3)
-CFGS=(1.4)
-N_CLASSIFIERS=1
+SOLVERS=("DS-Solver_Flow")
+NFES=(3 4 5 6)
+CFGS=(4.5)
 
 # 사용 GPU 개수 -> nproc
 IFS=',' read -ra _GPU_IDS <<< "$CUDA_VISIBLE_DEVICES"
@@ -34,8 +33,8 @@ BASE_OUT="samplings"   # SAVE_ROOT의 베이스
 for solver in "${SOLVERS[@]}"; do
   for nfe in "${NFES[@]}"; do
     for cfg in "${CFGS[@]}"; do
-      SAVE_ROOT="${BASE_OUT}/${MODEL}/${cfg}/${nfe}/${solver}/${N_SAMPLES}/${N_CLASSIFIERS}"
-      PT_DIR="logs/gmdit/multi/s${nfe}_n${N_CLASSIFIERS}"
+      SAVE_ROOT="${BASE_OUT}/${MODEL}/${cfg}/${nfe}/${solver}/${N_SAMPLES}"
+      PT_DIR="logs/sana/ds/s${nfe}"
       echo "▶ torchrun (nproc=${NPROC}, GPUs=${CUDA_VISIBLE_DEVICES}) | ${MODEL} | solver=${solver} | NFE=${nfe} | CFG=${cfg}"
       CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES \
       DIST_BACKEND=gloo torchrun --standalone --nproc_per_node="${NPROC}" \
@@ -56,7 +55,8 @@ for solver in "${SOLVERS[@]}"; do
           --n_samples "$N_SAMPLES" \
           --seed_offset "$SEED_OFFSET" \
           --batch_size "$BATCH_SIZE" \
-          --output_inception
+          --output_inception \
+          --output_clip_score
     done
   done
 done
