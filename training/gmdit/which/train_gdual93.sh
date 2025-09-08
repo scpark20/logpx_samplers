7705d1c17b2f64e531c360d1db03f0972586b7aa
scpark <scpark20@gmail.com>
2025-09-08 10:25:48 +0900

    sh

--- training/gmdit/which/train_gdual93.sh
+++ training/gmdit/which/train_gdual93.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 export DPM_TQDM=${DPM_TQDM:-False}
-export CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES:-3}
+export CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES:-0}
 
 STEPS=(9)
 N_CLASSIFIERS=(3 7 11 15 19)
