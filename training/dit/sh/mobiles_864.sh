2085d46fe5f088dcdcbec4183ea1b8f66e91bfa5
scpark <scpark20@gmail.com>
2025-09-13 08:19:25 +0900

    sh

---
+++ training/dit/sh/mobiles_864.sh
@@ -0,0 +1,3 @@
+./training/dit/mobile/train_gdual864.sh
+./training/dit/bns_mobile/train_bns864.sh
+./training/dit/ds_mobile/train_ds864.sh
---
+++ training/gmdit/sh/mobiles_864.sh
@@ -0,0 +1,3 @@
+./training/gmdit/mobile/train_gdual864.sh
+./training/gmdit/bns_mobile/train_bns864.sh
+./training/gmdit/ds_mobile/train_ds864.sh
