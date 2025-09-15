e8e181754e605bebc466252762acd59063845e5f
scpark <scpark20@gmail.com>
2025-09-15 13:30:08 +0900

    sh

--- training/sana25/rn/train.sh
+++ training/sana25/rn/train.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 export DPM_TQDM=${DPM_TQDM:-False}
 export CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES:-0}
 
-STEPS=(2 3 4 5 6)
+STEPS=(2)
 
 for s in "${STEPS[@]}"; do
   
---
+++ training/sana25/sh/rn_2steps.sh
@@ -0,0 +1,3 @@
+./training/sana25/rn/train.sh
+./training/sana25/bns_rn/train.sh
+./training/sana25/ds_rn/train.sh
