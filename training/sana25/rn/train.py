85db4b6461030cedd84db5a1d5837dd714f81c78
scpark <scpark20@gmail.com>
2025-09-15 13:37:02 +0900

    10k

--- training/sana25/rn/train.py
+++ training/sana25/rn/train.py
@@ -37,7 +37,7 @@ config.latent_size   = (32, 16, 16)
 # LR & Scheduler
 config.base_lr       = 2e-3
 config.end_lr        = 1e-4
-config.total_steps   = 20*1000        # 전체 학습 스텝
+config.total_steps   = 10*1000        # 전체 학습 스텝
 
 # ---- 여기만 CLI로 덮어씀 ----
 config.n_steps       = args.n_steps
