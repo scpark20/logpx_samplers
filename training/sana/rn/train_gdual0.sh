304d82b7e1d73ab9f5dafc5db7cc547773498e53
scpark <scpark20@gmail.com>
2025-10-29 17:05:46 +0900

    87

--- training/sana/rn/train_gdual0.sh
+++ training/sana/rn/train_gdual0.sh
@@ -3,7 +3,8 @@ set -euo pipefail
 export DPM_TQDM=${DPM_TQDM:-False}
 export CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES:-0}
 
-STEPS=(9 8 7)
+#STEPS=(9 8 7)
+STEPS=(8 7)
 
 for s in "${STEPS[@]}"; do
   
