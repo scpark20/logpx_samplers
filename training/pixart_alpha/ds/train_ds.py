2bd6f5f522f8837e9596b9609931eda8bd3ec953
scpark <scpark20@gmail.com>
2025-10-30 16:44:39 +0900

    check

--- training/pixart_alpha/ds/train_ds.py
+++ training/pixart_alpha/ds/train_ds.py
@@ -78,7 +78,7 @@ solver = DS_Solver(noise_schedule,
         config.n_steps,
         skip_type='time_uniform',
         algorithm_type='data_prediction',
-        checkpoint=False).to(device)
+        checkpoint=True).to(device)
 optimizer = torch.optim.AdamW(solver.parameters(), lr=config.base_lr)
 
 # ---- Scheduler: Pure Cosine ----
