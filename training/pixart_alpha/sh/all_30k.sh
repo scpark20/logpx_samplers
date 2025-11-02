438ae8700696b224270384e0c424307b11dbbedd
scpark <scpark20@gmail.com>
2025-11-02 10:19:55 +0900

    38 47 56

--- training/pixart_alpha/sh/all_30k.sh
+++ training/pixart_alpha/sh/all_30k.sh
@@ -1,5 +1,5 @@
 #bash training/pixart_alpha/sh/euler_30k.sh
 #bash training/pixart_alpha/sh/dpm_30k.sh
-# bash training/pixart_alpha/bns/sample_30k.sh
-# bash training/pixart_alpha/ds/sample_30k.sh
-bash training/pixart_alpha/rn/sample_30k.sh
+bash training/pixart_alpha/ds/sample_30k.sh
+bash training/pixart_alpha/bns/sample_30k.sh
+#bash training/pixart_alpha/rn/sample_30k.sh
