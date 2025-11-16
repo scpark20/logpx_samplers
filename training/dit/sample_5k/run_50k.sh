9b0b8a10e5dba6f1ba030855cc3104ba49bc7ef7
scpark <scpark20@gmail.com>
2025-11-16 23:53:40 +0900

    sample

--- training/dit/sample_5k/run_50k.sh
+++ training/dit/sample_5k/run_50k.sh
@@ -2,14 +2,14 @@
 set -e
 
 # 여기서 GPU 번호 수동 지정 (여러 개면 쉼표로)
-export CUDA_VISIBLE_DEVICES=0,1
+export CUDA_VISIBLE_DEVICES=0
 
 # 🔇 torchrun OMP 배너 억제(사전에 지정)
 export OMP_NUM_THREADS=${OMP_NUM_THREADS:-4}
 export MKL_NUM_THREADS=${MKL_NUM_THREADS:-4}
 export OPENBLAS_NUM_THREADS=${OPENBLAS_NUM_THREADS:-4}
 
-TAG=lpips_vgg
+TAG=traj
 MODEL=DiT
 DATA=ImageNet
 BATCH_SIZE=10          # GPU당 배치
@@ -20,8 +20,8 @@ N_SAMPLES=50000
 SEED_OFFSET=0
 
 SOLVERS=("Dual-Solver")
-NFES=(9 7 5 3)
-#NFES=(6 4)
+#NFES=(9 7 5 3)
+NFES=(6 4)
 CFGS=(1.5)
 
 # 사용 GPU 개수 -> nproc
@@ -34,7 +34,7 @@ for solver in "${SOLVERS[@]}"; do
   for nfe in "${NFES[@]}"; do
     for cfg in "${CFGS[@]}"; do
       SAVE_ROOT="${BASE_OUT}/${MODEL}/${cfg}/${nfe}/${solver}/${N_SAMPLES}"
-      PT_DIR="logs/dit/lpips_vgg/s${nfe}"
+      PT_DIR="logs/dit/traj/s${nfe}"
       echo "▶ torchrun (nproc=${NPROC}, GPUs=${CUDA_VISIBLE_DEVICES}) | ${MODEL} | solver=${solver} | NFE=${nfe} | CFG=${cfg}"
       CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES \
       DIST_BACKEND=gloo torchrun --standalone --nproc_per_node="${NPROC}" \
---
+++ training/dit/sh/dit_logit_15.sh
@@ -0,0 +1,2 @@
+bash ./training/dit/mobile_15/sample_50k.sh
+bash ./training/dit/mobile_logit/sample_50k.sh
